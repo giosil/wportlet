f983ed6fd2d1a53c3a80e3a0288789be27622e07
giosil <giorgio.silvestris@gmail.com>
2024-04-16 11:00:48 +0200

    update

--- deploy.cmd
+++ deploy.cmd
@@ -2,6 +2,10 @@
 
 set LIFERAY_CONT_NAME=csr-liferay
 
+rem docker pull liferay/portal:7.1.3-ga4
+
+rem docker run --name %LIFERAY_CONT_NAME% -p 8080:8080 -d liferay/portal:7.1.3-ga4
+
 docker cp ./target/wportlet.war %LIFERAY_CONT_NAME%:/opt/liferay
 
 docker exec -u root %LIFERAY_CONT_NAME% chown liferay:liferay /opt/liferay/wportlet.war
